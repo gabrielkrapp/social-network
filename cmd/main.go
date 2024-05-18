af798642d141fda81f4b8fb4db449606423cb978
Gabriel <gabriel.krapp@hotmail.com>
2024-05-18 19:23:13 -0300

    feat: Adding server config in main

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"log"
+	"social-network/config"
+)
+
 func main() {
+
+	server := config.NewHTTPServer(":8080")
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 
 }
